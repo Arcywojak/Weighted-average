dd3324b190eb200f876a6df565a1b251477099ec
Arcywojak <51534164+Arcywojak@users.noreply.github.com>
2019-06-17 14:14:20 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <windows.h>
+#include <vector>
+#include <fstream>
+#include "functions.h"
+
+using namespace std;
+
+void licz(string przedmiot);
+void stworz();
+void wybierz();
+void menu();
+void wczytaj_zapis(vector<string>&przedmioty);
+///////////////////////////
+int main()
+{
+
+    menu();
+
+}
+///////////////////////////
+
